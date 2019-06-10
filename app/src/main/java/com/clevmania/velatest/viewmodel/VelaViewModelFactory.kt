d0ee05cad3f4aaa48bd5530b897bfb1fe91b0d94
clevmania <ernestcleverest@gmail.com>
2019-06-10 17:07:03 +0100

    implemented viewmodel factory

--- app/src/main/java/com/clevmania/velatest/viewmodel/VelaViewModelFactory.kt
+++ app/src/main/java/com/clevmania/velatest/viewmodel/VelaViewModelFactory.kt
@@ -1,4 +1,12 @@
 package com.clevmania.velatest.viewmodel
 
-class VelaViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.clevmania.velatest.data.repository.VelaRepository
+
+class VelaViewModelFactory(private val velaRepository: VelaRepository): ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return VelaViewModel(velaRepository) as T
+    }
 }
