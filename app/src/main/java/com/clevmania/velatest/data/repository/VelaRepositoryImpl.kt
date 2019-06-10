e7f4a2f4311de4395b2b8742b96323adfac34980
clevmania <ernestcleverest@gmail.com>
2019-06-10 16:58:27 +0100

    created repository implementation

--- app/src/main/java/com/clevmania/velatest/data/repository/VelaRepositoryImpl.kt
+++ app/src/main/java/com/clevmania/velatest/data/repository/VelaRepositoryImpl.kt
@@ -1,13 +1,24 @@
 package com.clevmania.velatest.data.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.clevmania.velatest.data.model.CommitResponse
+import com.clevmania.velatest.data.network.VelaDataSource
 
-class VelaRepositoryImpl : VelaRepository {
+class VelaRepositoryImpl(private val velaDataSource: VelaDataSource) : VelaRepository {
+    private var allCommits = MutableLiveData<List<CommitResponse>>()
+
     override suspend fun retrieveCommits(): LiveData<List<CommitResponse>> {
-
+        return allCommits
     }
 
     override suspend fun fetchDevelopersCommit(page: String, commitsPerPage: String) {
+        velaDataSource.fetchDevelopersCommits(page,commitsPerPage)
+    }
+
+    init {
+        velaDataSource.retrieveDevsCommits.observeForever {
+            allCommits.value = it
+        }
     }
 }
