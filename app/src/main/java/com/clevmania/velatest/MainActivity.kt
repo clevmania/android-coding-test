2fc5c1c801f75ebe061fd28bdf7efdd4e99ac2a4
clevmania <ernestcleverest@gmail.com>
2019-06-10 17:49:44 +0100

    rewired mainactivity - setup activity with view model

--- app/src/main/java/com/clevmania/velatest/MainActivity.kt
+++ app/src/main/java/com/clevmania/velatest/MainActivity.kt
@@ -2,27 +2,49 @@ package com.clevmania.velatest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.clevmania.velatest.data.model.CommitModel
 import com.clevmania.velatest.data.network.VelaDataSourceImpl
 import com.clevmania.velatest.data.network.api.VelaApiService
 import com.clevmania.velatest.data.network.interceptor.ConnectivityInterceptorImpl
+import com.clevmania.velatest.data.repository.VelaRepositoryImpl
 import com.clevmania.velatest.ui.CommitsAdapter
+import com.clevmania.velatest.viewmodel.VelaViewModel
+import com.clevmania.velatest.viewmodel.VelaViewModelFactory
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private lateinit var viewModel : VelaViewModel
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loadCommits()
+//        loadCommits()
+        initFactory()
+        bindUiToViewModel()
     }
 
+    private fun initFactory(){
+        job = Job()
+        val apiService = VelaApiService(ConnectivityInterceptorImpl(this))
+
+        val repository = VelaRepositoryImpl(VelaDataSourceImpl(apiService, applicationContext))
+        val viewModelFactory = VelaViewModelFactory(repository)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(VelaViewModel::class.java)
+    }
+/*
     private fun loadCommits(){
         val commitsList = arrayListOf<CommitModel>()
         pb_commit.visibility = View.VISIBLE
@@ -58,7 +80,43 @@ class MainActivity : AppCompatActivity() {
         GlobalScope.launch {
             velaDataSource.fetchDevelopersCommits("1","25")
         }
+    }*/
+
+    private fun bindUiToViewModel() = launch {
+        val commitsList = arrayListOf<CommitModel>()
+        pb_commit.visibility = View.VISIBLE
+        viewModel.retrieveAllCommits("1","25")
+
+        val devsCommit = viewModel.allDevelopersCommit.await()
+        devsCommit.observe(this@MainActivity, Observer {
+            if(it == null){
+                pb_commit.visibility = View.GONE
+                tv_status.text = getString(R.string.cannot_fetch)
+                return@Observer
+            }
+
+            if(it.isNullOrEmpty()){
+                tv_status.text = getString(R.string.no_commits_found)
+                pb_commit.visibility = View.GONE
+                return@Observer
+            }
+
+            pb_commit.visibility = View.GONE
+            tv_status.visibility = View.GONE
+
+            it.forEach {devsCommit->
+                commitsList.add(CommitModel(devsCommit.commit.author.name, devsCommit.sha, devsCommit.commit.message))
+            }
+
+            rv_commits.setHasFixedSize(true)
+            rv_commits.layoutManager = LinearLayoutManager(this@MainActivity)
+            rv_commits.adapter = CommitsAdapter(commitsList)
+        })
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
 
 }
